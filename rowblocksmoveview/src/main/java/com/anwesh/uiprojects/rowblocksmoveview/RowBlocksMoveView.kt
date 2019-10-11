95a6c345acdf39b5be7ecf4bde02d2f5e32f86a6
Anwesh43 <anweshthecool0@gmail.com>
2019-10-11 09:54:27 +0530

    adding RowBlocksMoveView class

--- rowblocksmoveview/src/main/java/com/anwesh/uiprojects/rowblocksmoveview/RowBlocksMoveView.kt
+++ rowblocksmoveview/src/main/java/com/anwesh/uiprojects/rowblocksmoveview/RowBlocksMoveView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawRBMNode(i : Int, scale : Float, paint : Paint) {
     drawRowBlock(scale, size, w, paint)
     restore()
 }
+
+class RowBlocksMoveView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
