6e548c69439a89493948e3ab2c16eb2a9b91ebd7
Anwesh43 <anweshthecool0@gmail.com>
2019-10-11 10:08:10 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrowblocksmoveview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrowblocksmoveview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrowblocksmoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rowblocksmoveview.RowBlocksMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RowBlocksMoveView.create(this)
     }
 }
